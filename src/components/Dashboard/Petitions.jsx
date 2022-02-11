51a89c97333a19a98c872d1e0ca460b3980b65bc
Daniel Batista <danielbped@gmail.com>
2022-02-11 12:07:02 -0300

    Component Loading

--- src/components/Dashboard/Petitions.jsx
+++ src/components/Dashboard/Petitions.jsx
@@ -1,18 +1,23 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
-import PetitionCard from './PetitionCard'
+import PetitionCard from './PetitionCard';
+import Loading from '../Loading';
 
 const Petitions = ({ page }) => {
   const authorization = localStorage.getItem('token');
   const [petitions, setPetitions] = useState([])
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios({
       url: `http://localhost:8080/peticoes?_page=${page}&_limit=2`,
       method: 'GET',
       headers: { authorization },
-    }).then(({ data }) => setPetitions(data))
+    }).then(({ data }) => setPetitions(data));
+    setLoading(false);
   }, [page, authorization])
+
+  if (loading) return <Loading />
 
   return (
     <div>
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <p>Carregando ...</p>
+  );
+};
+
+export default Loading;
